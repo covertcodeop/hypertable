995e766116371611eadf3afd1e5fb4aa30d76030
Zenko B. Klapko Jr <zklapko@gmail.com>
2016-03-15 22:25:34 -0400

    Support directory for Ubuntu 12.04

--- bin/src-utils/build-setup-ubuntu12.04.sh
+++ bin/src-utils/build-setup-ubuntu12.04.sh
@@ -44,6 +44,8 @@ apt-get -y --allow-unauthenticated install openjdk-6-jdk maven
 
 apt-get -y --allow-unauthenticated install ant autoconf automake libtool bison flex pkg-config php5 php5-dev php5-cli ruby ruby-dev python-dev libhttp-access2-ruby libbit-vector-perl libclass-accessor-chained-perl nodejs
 
+mkdir ../../support
+cd ../../support
 # Going to comment out and use the test ppa for gcc-5.x
 # Need the following prerequisties to build gcc52
 #cd ~
@@ -53,14 +55,14 @@ apt-get -y --allow-unauthenticated install ant autoconf automake libtool bison f
 #./configure
 #make
 #make install
-#cd ~
+#cd ../
 #wget http://mirrors.ibiblio.org/gnu/ftp/gnu/mpfr/mpfr-3.1.3.tar.bz2
 #tar -jxf mpfr-3.1.3.tar.bz2
 #cd mpfr-3.1.3/
 #./configure
 #make
 #make install
-#cd ~
+#cd ../
 #wget http://mirrors.ibiblio.org/gnu/ftp/gnu/mpc/mpc-1.0.3.tar.gz
 #tar -zxf mpc-1.0.3.tar.gz
 #cd mpc-1.0.3/
@@ -81,26 +83,24 @@ apt-get -y --allow-unauthenticated install ant autoconf automake libtool bison f
 #/sbin/ldconfig
 #
 # Boost
-cd ~
 wget http://downloads.sourceforge.net/boost/boost_1_57_0.tar.bz2
 tar xjvf boost_1_57_0.tar.bz2
 cd boost_1_57_0
 #Safer to build all libraries as other projects rely upon aspects of boost
 ./bootstrap.sh --with-libraries=all
 ./bjam install
-#cd ~; /bin/rm -rf ~/boost_1_55_0*
+cd ../
 
 # SIGAR
-cd ~
 git clone git://github.com/hyperic/sigar.git
 cd sigar
 #Need the cppflag because of change in inline behavior
 #https://github.com/hyperic/sigar/issues/60
 ./autogen.sh && ./configure CPPFLAGS='-fgnu89-inline' && make && make install
 /sbin/ldconfig
+cd ../
 
 # BerkeleyDB
-cd ~
 wget http://www.hypertable.com/uploads/db-4.8.26.tar.gz
 tar -xzvf db-4.8.26.tar.gz
 cd db-4.8.26/build_unix/
@@ -108,10 +108,9 @@ cd db-4.8.26/build_unix/
 make
 make install
 sh -c "echo '/usr/local/BerkeleyDB.4.8/lib' > /etc/ld.so.conf.d/BerkeleyDB.4.8.conf"
-cd ~; /bin/rm -rf ~/db-4.8.26*
+cd ../
 
 # Google RE2
-cd ~
 wget http://www.hypertable.com/uploads/re2.tgz
 tar -zxvf re2.tgz
 cd re2
@@ -120,23 +119,20 @@ sed 's:<string.h>:<string.h>\
 #include <cstddef>:' re2/stringpiece.h > sp.bak && mv sp.bak re2/stringpiece.h
 make
 make install
-#/bin/rm -rf ~/re2*
+cd ../
 
 # libunwind
 if [ $ARCH -eq 64 ]; then
-  cd ~
   wget http://download.savannah.gnu.org/releases/libunwind/libunwind-1.0.1.tar.gz
   tar xzvf libunwind-1.0.1.tar.gz 
   cd libunwind-1.0.1/
   ./configure CFLAGS=-U_FORTIFY_SOURCE
   make
   make install
-  cd ~
-  /bin/rm -rf ~/libunwind-1.0.1*
+  cd ../
 fi
 
 # Google Perftools
-cd ~
 wget http://google-perftools.googlecode.com/files/google-perftools-1.8.3.tar.gz
 tar xzvf google-perftools-1.8.3.tar.gz
 cd google-perftools-1.8.3
@@ -144,41 +140,34 @@ sed -i -e 's/siginfo_t/siginfo/' src/base/linuxthreads.cc
 ./configure
 make
 make install
-cd ~
-#/bin/rm -rf ~/google-perftools-1.8.3*
-
+cd ../
 
 # Google Snappy
-cd ~
 wget http://snappy.googlecode.com/files/snappy-1.0.4.tar.gz
 tar xzvf snappy-1.0.4.tar.gz
 cd snappy-1.0.4
 ./configure
 make
 make install
-cd ~
-/bin/rm -rf ~/snappy-1.0.4*
-
+cd ../
 
 # libevent4
-#cd ~
 #wget https://github.com/downloads/libevent/libevent/libevent-1.4.14b-stable.tar.gz
 #tar xzvf libevent-1.4.14b-stable.tar.gz 
 #cd libevent-1.4.14b-stable
 #./configure 
 #make
 #make install
-#cd ~; rm -rf libevent-1.4.14b-stable*
+#cd ../
 
 # libedit -need to compile separately for wide character support
-cd ~
 wget http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz
 tar -zxf libedit-20150325-3.1.tar.gz
 cd libedit-20150325-3.1
 ./configure --enable-widec
 make
 make install
-cd ~
+cd ../
 
 wget https://github.com/libevent/libevent/archive/master.zip
 unzip master.zip
@@ -187,21 +176,21 @@ cd libevent-master
 ./configure
 make
 make install
-cd ~;
+cd ../
 
 
 # Scrooge (Scala Thrift binding)
-wget http://apt.typesafe.com/repo-deb-build-0002.deb
-dpkg -i repo-deb-build-0002.deb
-apt-get update
-apt-get -y --force-yes --fix-missing install sbt
-mkdir -p ~/.sbt/.lib/0.11.2
-cd ~/.sbt/.lib/0.11.2
-wget http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/0.11.2/sbt-launch.jar
-cd /usr/src
-git clone git://github.com/twitter/scrooge.git
-cd scrooge
-sbt -sbt-version 0.11.2 package-dist
+#wget http://apt.typesafe.com/repo-deb-build-0002.deb
+#dpkg -i repo-deb-build-0002.deb
+#apt-get update
+#apt-get -y --force-yes --fix-missing install sbt
+#mkdir -p .sbt/.lib/0.11.2
+#cd .sbt/.lib/0.11.2
+#wget http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/0.11.2/sbt-launch.jar
+#git clone git://github.com/twitter/scrooge.git
+#cd scrooge
+#sbt -sbt-version 0.11.2 package-dist
+#cd ../../../
 
 apt-get -y --allow-unauthenticated install dstat doxygen rrdtool graphviz gdb emacs rdoc rubygems-integration capistrano
 
@@ -209,9 +198,6 @@ apt-get -y --allow-unauthenticated install dstat doxygen rrdtool graphviz gdb em
 gem install sinatra rack thin json titleize bundle
 
 # Thrift
-#Must be version 0.9.2, 0.9.3+ breaks the API HT uses with incrementRecursionDepth
-#splitting from half-duplex to fullDuplex.
-cd ~
 #Mirror died?
 #wget http://mirror.olnevhost.net/pub/apache/thrift/0.9.2/thrift-0.9.2.tar.gz
 wget http://apache.mirror.anlx.net/thrift/0.9.3/thrift-0.9.3.tar.gz
@@ -221,10 +207,10 @@ cd thrift-0.9.3
 ./configure --without-java
 make
 make install
+cd ../
 
 # Ubuntu 12.04 has an older version of libssh. It's necessary to download,
 # alter the code for GCC 5.2, and make install
-cd ~
 wget --no-check-certificate https://red.libssh.org/attachments/download/81/libssh-0.6.1.tar.xz
 tar -xf libssh-0.6.1.tar.xz
 cd libssh-0.6.1
@@ -235,12 +221,12 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ..
 make
 make install
+cd ../../
 
-cd ~
 git clone https://github.com/jemalloc/jemalloc.git
 cd jemalloc
 ./autogen.sh && ./configure && make && make install
-cd ~
+cd ../
 
 /sbin/ldconfig
 
